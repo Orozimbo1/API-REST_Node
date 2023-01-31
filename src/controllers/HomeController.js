63d4cd7cd871293bf74fc563e7b37bf34b9c77c3
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-01-31 19:34:01 -0300

    Rotas de alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Matheus',
-      sobrenome: 'Orozimbo',
-      email: 'matheus@matheus.com',
-      idade: 24,
-      peso: 99.9,
-      altura: 1.80,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
