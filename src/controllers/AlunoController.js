3f4b5b8e2267d0ba34300f4427829ae094b83ee1
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-01-31 20:11:47 -0300

    CRUD de alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -3,7 +3,99 @@ import Aluno from '../models/Aluno';
 class AlunoController {
   async index(req, res) {
     const alunos = await Aluno.findAll();
-    res.json(alunos);
+    return res.json(alunos);
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          erros: ['Faltando o ID.'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          erros: ['Aluno não existe.'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          erros: ['Faltando o ID.'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          erros: ['Aluno não existe.'],
+        });
+      }
+
+      const alunoAtualizado = await aluno.update(req.body);
+      return res.json(alunoAtualizado);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          erros: ['Faltando o ID.'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          erros: ['Aluno não existe.'],
+        });
+      }
+
+      await aluno.destroy();
+      return res.json(`Aluno ${aluno.nome} ${aluno.sobrenome} apagado com sucesso.`);
+    } catch (e) {
+      return res.status(400).json({
+        erros: e.errors.map((err) => err.message),
+      });
+    }
   }
 }
 
