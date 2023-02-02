c16b7ec3b6b306ce599f2cdfea2183254aa3561c
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-01 21:39:22 -0300

    loginRequired

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -1,7 +1,7 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
 
 var _UserController = require('../controllers/UserController'); var _UserController2 = _interopRequireDefault(_UserController);
-var _LoginRequired = require('../middleware/LoginRequired'); var _LoginRequired2 = _interopRequireDefault(_LoginRequired);
+var _loginRequired = require('../middleware/loginRequired'); var _loginRequired2 = _interopRequireDefault(_loginRequired);
 
 const router = new (0, _express.Router)();
 
@@ -10,7 +10,7 @@ router.get('/', _UserController2.default.index);
 // router.get('/:id', userController.show);
 
 router.post('/', _UserController2.default.store);
-router.put('/', _LoginRequired2.default, _UserController2.default.update);
-router.delete('/', _LoginRequired2.default, _UserController2.default.delete);
+router.put('/', _loginRequired2.default, _UserController2.default.update);
+router.delete('/', _loginRequired2.default, _UserController2.default.delete);
 
 exports. default = router;
